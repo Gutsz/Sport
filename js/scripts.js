06cfae91b5d29ffb0145825bf192e677ae9475c5
Ihor Anreichenko <nexus7210994@gmail.com>
2019-12-05 13:53:24 +0200

    final block and responsive

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@ $('.navigation').on('click', '.menu-icon', function ()
 {
     $('.navigation-container').slideToggle(400);
 });
+
+jQuery(document).ready(function () {
+
+            $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+                $(this)
+                    .addClass('active').siblings().removeClass('active')
+                    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            });
+
+        });
